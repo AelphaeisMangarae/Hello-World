a1c650d916fe6053256d123e2a151051aae7f17f
Joshua <Joshua.hughes08@gmail.com>
2018-02-04 14:07:11 -0800

    print date

--- src/hello/world/HelloWorld.java
+++ src/hello/world/HelloWorld.java
@@ -1,7 +1,9 @@
 package hello.world;
+import java.util.Date;
 
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HIIIIIII");
+        System.out.println(new Date());
     }
 }
